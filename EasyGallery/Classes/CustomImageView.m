933a847d4cae5d8d60f896a27870b1c7fa2078c5
Hiroshi Hashiguchi <xcatsan@mac.com>
2010-09-29 15:41:48 +0900

    Updated EasyGallery

--- EasyGallery/Classes/CustomImageView.m
+++ EasyGallery/Classes/CustomImageView.m
@@ -16,4 +16,42 @@
 	return self;
 }
 
+-(id)initWithFrame:(CGRect)frame
+{
+	if (self = [super initWithFrame:frame]) {
+		[self setUserInteractionEnabled:YES];
+	}
+	return self;
+}
+
+
+- (CGRect)zoomRectForScrollView:(UIScrollView *)scrollView
+					  withScale:(float)scale withCenter:(CGPoint)center {
+	
+    CGRect zoomRect;
+    zoomRect.size.height = scrollView.frame.size.height / scale;
+    zoomRect.size.width  = scrollView.frame.size.width  / scale;
+	zoomRect.origin.x = center.x - (zoomRect.size.width  / 2.0);
+    zoomRect.origin.y = center.y - (zoomRect.size.height / 2.0);
+	
+    return zoomRect;
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+	UITouch* touch = [touches anyObject];
+	if ([touch tapCount] == 2) {
+		UIScrollView* scrollView = (UIScrollView*)self.superview;
+
+		CGRect zoomRect;
+		if (scrollView.zoomScale > 1.0) {
+			zoomRect = scrollView.bounds;
+		} else {
+			zoomRect = [self zoomRectForScrollView:scrollView
+										 withScale:2.0
+										withCenter:[touch locationInView:nil]];
+		}
+		[scrollView zoomToRect:zoomRect animated:YES];
+	}
+}
 @end
